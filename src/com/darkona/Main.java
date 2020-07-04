580dfc1f8b9dd086fa33aa7cc9f5d44f3b57b8dd
Darkona <Javier.Darkona@gmail.com>
2020-07-03 21:44:23 -0400

    Count to 100

--- src/com/darkona/Main.java
+++ src/com/darkona/Main.java
@@ -7,5 +7,17 @@ public class Main {
         System.out.println("Hello Cheetah!");
         System.out.println("Line added by Darkona");
         System.out.println("La pizza lleva piña");
+        int i = 0;
+
+        StringBuilder str = new StringBuilder();
+
+        while (i < 100){
+            if(i<9) str.append(0);
+            str.append(++i);
+            if(i<100) str.append(",");
+            if(i % 10 == 0 ) str.append("\n");
+        }
+
+        System.out.println(str);
     }
 }
